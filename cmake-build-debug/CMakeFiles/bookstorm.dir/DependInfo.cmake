0fcd42996f25f9a14ed59949850ba15bd342c9b3
z-taylcr7 <554795808@qq.com>
2021-12-13 09:08:44 +0800

    v1.0.3

--- cmake-build-debug/CMakeFiles/bookstorm.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/bookstorm.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/mnt/c/Users/Cristiano/CLionProjects/bookstorm/main.cpp" "/mnt/c/Users/Cristiano/CLionProjects/bookstorm/cmake-build-debug/CMakeFiles/bookstorm.dir/main.cpp.o"
+  "/mnt/d/book/main.cpp" "/mnt/d/book/cmake-build-debug/CMakeFiles/bookstorm.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
